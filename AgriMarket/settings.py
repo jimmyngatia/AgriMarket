f846891e7978c7b1c4e3da4dcf2a1ac26fc4f03e
jimmyngatia <ngatiajimmy010@gmai.com>
2025-07-31 19:09:42 +0300

    ...

--- AgriMarket/settings.py
+++ AgriMarket/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'agrimarket-41fk.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
